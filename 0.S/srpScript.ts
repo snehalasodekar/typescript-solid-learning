ebeeacf2c2ef7c250f70cba58362d822b1fe12ce
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-29 16:46:45 +0200

    create separate ts file for SRP principle

---
+++ 0.S/srpScript.ts
@@ -0,0 +1,12 @@
+/**
+ * By referring code from old.ts, I need to clean it on the basis of implementing Single responsibility principle.
+ * create a class Car
+ */
+const musicToggleElement = <HTMLElement>document.querySelector('#music-toggle');
+const musicSliderElement = <HTMLInputElement>document.querySelector('#music-slider');
+const engineToggleElement = <HTMLInputElement>document.querySelector('#engine-toggle');
+const addFuelForm = document.querySelector('#add-fuel-form');
+const addFuelInput = <HTMLFormElement>document.querySelector('#add-fuel-input');
+const fuelLevelElement = <HTMLElement>document.querySelector('#fuel-level');
+const milesElement = <HTMLElement>document.querySelector('#miles-value');
+const audioElement = <HTMLAudioElement>document.querySelector('#car-music');
